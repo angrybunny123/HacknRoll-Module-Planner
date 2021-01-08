dde56da87f2eecfd7bd72bb7f25a809be650b394
angrybunny123 <kennethteo98@gmail.com>
2021-01-08 17:54:53 +0800

    Linked firebase data

--- src/components/ModuleContainer/ModuleContainer.js
+++ src/components/ModuleContainer/ModuleContainer.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "../../axios.js";
 
 import classes from "./ModuleContainer.module.css";
 import { TextField, InputLabel, Select, MenuItem } from "@material-ui/core";
@@ -9,151 +10,230 @@ import Card from "../Card";
 import Board from "../Board";
 
 const onDropHandler = (list, startIndex, endIndex) => {
-    console.log(list);
-    const result = Array.from(list);
-    const [removed] = result.splice(startIndex, 1);
-    result.splice(endIndex, 0, removed);
-    console.log(result);
-    return result;
+  console.log(list);
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+  console.log(result);
+  return result;
 };
 
 const getItemStyle = (isDragging, draggableStyle) => ({
-    // some basic styles to make the items look a bit nicer
-    userSelect: "none",
-    padding: 8 * 2,
-    margin: `0 ${8}px 0 0`,
-    height: `20px`,
+  // some basic styles to make the items look a bit nicer
+  userSelect: "none",
+  padding: 8 * 2,
+  margin: `0 ${8}px 0 0`,
+  height: `20px`,
 
-    // change background colour if dragging
-    background: isDragging ? "lightgreen" : "grey",
+  // change background colour if dragging
+  background: isDragging ? "lightgreen" : "grey",
 
-    // styles we need to apply on draggables
-    ...draggableStyle,
+  // styles we need to apply on draggables
+  ...draggableStyle,
 });
 
 const getListStyle = (isDraggingOver) => ({
-    background: isDraggingOver ? "lightblue" : "lightgrey",
-    display: "flex",
-    padding: 8,
-    overflow: "auto",
+  background: isDraggingOver ? "lightblue" : "lightgrey",
+  display: "flex",
+  padding: 8,
+  overflow: "auto",
 });
 
 class ModuleContainer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            dropdown: ["Foundation", "Math and Science", "IT Professionalism"],
-            currentField: "",
-            modules: [
-                {
-                    code: "CS1231",
-                    prereq: null,
-                    preclusion: null,
-                },
-                {
-                    code: "CS1101S",
-                    prereq: null,
-                    preclusion: null,
-                },
-            ],
-        };
-        this.onDragEnd = this.onDragEnd.bind(this);
-    }
-
-    onModuleFieldHandler = (event) => {
-        this.setState({ currentField: event.target.value });
+  constructor(props) {
+    super(props);
+    this.state = {
+      dropdown: [
+        "Foundation",
+        "Math and Science",
+        "IT Professionalism",
+        "Team Project",
+        "Industrial Experience",
+        "University Level Requirements",
+        "Unrestricted Electives",
+        "Algorithms & Theory",
+        "Artificial Intelligence",
+        "Computer Graphics & Games",
+        "Computer Security",
+        "Database Systems",
+        "Multimedia Information Retrieval",
+        "Networking & Distributed Systems",
+        "Parallel Computing",
+        "Programming Languages",
+        "Software Engineering",
+      ],
+      currentField: "",
+      modules: [
+        {
+          code: "CS1231S",
+          credits: 4,
+          name: "Discrete Structures",
+          prerequisites: "none",
+          sem: 0,
+        },
+        {
+          code: "CS1101",
+          credits: 4,
+          name: "Programming Methodology",
+          prerequisites: "none",
+          sem: 0,
+        },
+      ],
     };
+    this.onDragEnd = this.onDragEnd.bind(this);
+  }
 
-    onDragEnd(result) {
-        if (!result.destination) {
-            return;
-        }
-
-        const newModules = onDropHandler(
-            this.state.modules,
-            result.source.index,
-            result.destination.index
-        );
-
-        this.setState({
+  onModuleFieldHandler = (event) => {
+    this.setState({ currentField: event.target.value });
+    let stringToPost = "";
+    if (event.target.value === "Foundation") {
+      stringToPost = "foundation";
+    } else if (event.target.value === "Math and Science") {
+      stringToPost = "mathscience";
+    } else if (event.target.value === "IT Professionalism") {
+      stringToPost = "itprofessionalism";
+    } else if (event.target.value === "Team Project") {
+      stringToPost = "teamproject";
+    } else if (event.target.value === "Industrial Experience") {
+      stringToPost = "industrial";
+      // } else if (event.target.value === "University Level Requirements") {
+      //     stringToPost = "/mathscience";
+      // } else if (event.target.value === "Unrestricted Electives") {
+      //     stringToPost = "/mathscience";
+    } else if (event.target.value === "Algorithms & Theory") {
+      stringToPost = "focusareas/algorithmsandtheory";
+    } else if (event.target.value === "Artificial Intelligence") {
+      stringToPost = "focusareas/artificialintelligence";
+    } else if (event.target.value === "Computer Graphics & Games") {
+      stringToPost = "focusareas/computergraphicsandgames";
+    } else if (event.target.value === "Computer Security") {
+      stringToPost = "focusareas/computersecurity";
+    } else if (event.target.value === "Database Systems") {
+      stringToPost = "focusareas/databasesystems";
+    } else if (event.target.value === "Multimedia Information Retrieval") {
+      stringToPost = "focusareas/multimedia";
+    } else if (event.target.value === "Networking & Distributed Systems") {
+      stringToPost = "focusareas/networkinganddistributedsystems";
+    } else if (event.target.value === "Parallel Computing") {
+      stringToPost = "focusareas/parallelcomputing";
+    } else if (event.target.value === "Programming Languages") {
+      stringToPost = "focusareas/programminglanguages";
+    } else if (event.target.value === "Software Engineering") {
+      stringToPost = "focusareas/softwareengineering";
+    }
+    axios
+      .get(`${stringToPost}.json`)
+      .then((response) => {
+        if (response.data.primaries !== undefined) {
+          let newModules = Object.values(response.data.primaries);
+          if (response.data.electives !== undefined) {
+            let electives = Object.values(response.data.electives);
+            for (let i = 0; i < electives.length; i++) {
+              newModules.push(electives[i]);
+            }
+          }
+          this.setState({
             modules: newModules,
-        });
+          });
+          console.log(newModules);
+        } else {
+          this.setState({
+            modules: Object.values(response.data),
+          });
+        }
+      })
+      .catch((error) => console.log(error));
+  };
+
+  onDragEnd(result) {
+    if (!result.destination) {
+      return;
     }
 
-    render() {
-        let modules;
-        modules = this.state.modules.map((module) => {
-            return (
-                <Board id={module.code} className="board">
-                    <Card id="card-1" className="card" draggable="true">
-                        <Module moduleCode={module.code} />
-                    </Card>
-                </Board>
-            );
-        });
+    const newModules = onDropHandler(
+      this.state.modules,
+      result.source.index,
+      result.destination.index
+    );
 
-        let moduleFields;
-        moduleFields = this.state.dropdown.map((field) => {
-            return (
-                <MenuItem key={field} value={field}>
-                    {field}
-                </MenuItem>
-            );
-        });
+    this.setState({
+      modules: newModules,
+    });
+  }
 
-        console.log(modules);
+  render() {
+    let modules;
+    modules = this.state.modules.map((module) => {
+      return (
+        <Board id={module.code} className="board">
+          <Card id="card-1" className="card" draggable="true">
+            <Module moduleCode={module.code} />
+          </Card>
+        </Board>
+      );
+    });
 
-        return (
-            <div className={classes.ModuleContainer}>
-                <div className={classes.Dropdown}>
-                    <TextField
-                        id="standard-select-field"
-                        select
-                        value={this.state.currentField}
-                        onChange={this.onModuleFieldHandler}
-                        helperText="Please select your area of interest"
-                    >
-                        {moduleFields}
-                    </TextField>
-                </div>
-                <DragDropContext onDragEnd={this.onDragEnd}>
-                    <Droppable droppableId="droppable" direction="horizontal">
-                        {(provided, snapshot) => (
-                            <div
-                                ref={provided.innerRef}
-                                style={getListStyle(snapshot.isDraggingOver)}
-                                {...provided.droppableProps}
-                            >
-                                {this.state.modules.map((module, index) => (
-                                    <Draggable
-                                        key={module.code}
-                                        draggableId={module.code}
-                                        index={index}
-                                    >
-                                        {(provided, snapshot) => (
-                                            <div
-                                                ref={provided.innerRef}
-                                                {...provided.draggableProps}
-                                                {...provided.dragHandleProps}
-                                                style={getItemStyle(
-                                                    snapshot.isDragging,
-                                                    provided.draggableProps
-                                                        .style
-                                                )}
-                                            >
-                                                {module.code}
-                                            </div>
-                                        )}
-                                    </Draggable>
-                                ))}
-                                {provided.placeholder}
-                            </div>
+    console.log(modules);
+
+    let moduleFields;
+    moduleFields = this.state.dropdown.map((field) => {
+      return (
+        <MenuItem key={field} value={field}>
+          {field}
+        </MenuItem>
+      );
+    });
+
+    return (
+      <div className={classes.ModuleContainer}>
+        <div className={classes.Dropdown}>
+          <TextField
+            id="standard-select-field"
+            select
+            value={this.state.currentField}
+            onChange={this.onModuleFieldHandler}
+            helperText="Please select your area of interest"
+          >
+            {moduleFields}
+          </TextField>
+        </div>
+        <DragDropContext onDragEnd={this.onDragEnd}>
+          <Droppable droppableId="droppable" direction="horizontal">
+            {(provided, snapshot) => (
+              <div
+                ref={provided.innerRef}
+                style={getListStyle(snapshot.isDraggingOver)}
+                {...provided.droppableProps}
+              >
+                {this.state.modules.map((module, index) => (
+                  <Draggable
+                    key={module.code}
+                    draggableId={module.code}
+                    index={index}
+                  >
+                    {(provided, snapshot) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        style={getItemStyle(
+                          snapshot.isDragging,
+                          provided.draggableProps.style
                         )}
-                    </Droppable>
-                </DragDropContext>
-            </div>
-        );
-    }
+                      >
+                        {module.code}
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+      </div>
+    );
+  }
 }
 
 export default ModuleContainer;
