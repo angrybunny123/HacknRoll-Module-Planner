1ae32cd1ccd17e341c75acfd6e2cdaa6849589e4
angrybunny123 <kennethteo98@gmail.com>
2021-01-08 12:22:55 +0800

    Added dependencies

--- src/App.js
+++ src/App.js
@@ -1,58 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import { Button } from '@material-ui/core';
+import axios from './axios.js';
 
-function App() {
-  return (
+import Board from './components/Board';
+import Card from './components/Card';
+
+class App extends Component {
+
+  axiosTestPostRequest = () => { 
+    const module = { 
+      credits: 4, 
+      name: "GES1035",
+      prerequisites: ["GEQ1000","GER1000"]
+    }
+    axios.post('/modulePost.json', module).then(response => console.log(response)).catch(error => console.log(error));
+  }
+
+  axiosTestGetRequest = () => { 
+    axios.get('modulePost.json').then(response => console.log(response)).catch(error => console.log(error));
+  }
+  render() {
+    return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Hackathon Module Planner Template</h2>
         <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
+        <Button color="primary" onClick={this.axiosTestPostRequest}>POST DUMMY MODULE</Button>
+        <Button color="primary" onClick={this.axiosTestGetRequest}>GET ALL MODULES</Button>
       </header>
+      <main className="flexbox">
+      <Board id="board-1" className="board">
+        <Card id="card-1" className="card" draggable="true">
+          <p>card one</p>
+        </Card>
+      </Board>
+      <Board id="board-2" className="board">
+        <Card id="card-2" className="card" draggable="true">
+          <p>card two</p>
+        </Card>
+      </Board>
+      <Board id="board-3" className="board">
+        <Card id="card-3" className="card" draggable="true">
+          <p>card three</p>
+        </Card>
+      </Board>
+      <Board id="board-4" className="board">
+        <Card id="card-4" className="card" draggable="true">
+          <p>card four</p>
+        </Card>
+      </Board>
+
+      </main>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
