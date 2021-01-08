f2428506e1e31cbffccbb1faa2702584bfa8823b
Lye Yi Xian <lyeyx999@hotmail.com>
2021-01-08 20:02:52 +0800

    no message

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -1,8 +1,6 @@
 import { Paper } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import React, { Component } from "react";
-import { useState } from "react";
-import classes from "./Summary.module.css";
+import React from "react";
 
 const useStyles = makeStyles((theme) => ({
     label: {
@@ -101,71 +99,62 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-class Summary extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            mcRem: 0,
-            mcNeeded: 0,
-        };
-    }
-
-    calculateMcNeeded() {
-        if (this.props.label === "Foundation") {
+const Summary = (props) => {
+    const calculateMcNeeded = () => {
+        if (props.label === "Foundation") {
             return 36;
-        } else if (this.props.label === "Focus Area") {
+        } else if (props.label === "Focus Area") {
             return 24;
-        } else if (this.props.label === "Math and Science") {
+        } else if (props.label === "Math and Science") {
             return 16;
-        } else if (this.props.label === "IT Professionalism") {
+        } else if (props.label === "IT Professionalism") {
             return 12;
-        } else if (this.props.label === "Industrial Experience") {
+        } else if (props.label === "Industrial Experience") {
             return 12;
-        } else if (this.props.label === "Team Project") {
+        } else if (props.label === "Team Project") {
             return 8;
-        } else if (this.props.label === "Unrestricted Electives") {
+        } else if (props.label === "Unrestricted Electives") {
             return 32;
-        } else if (this.props.label === "University Level Requirements") {
+        } else if (props.label === "University Level Requirements") {
             return 20;
         }
-    }
+    };
+    const classes = useStyles();
 
-    render() {
-        return (
-            <Paper
-                elevation={3}
-                className={
-                    this.props.label === "Foundation"
-                        ? classes.Foundation
-                        : this.props.label === "Focus Area"
-                        ? classes.FocusArea
-                        : this.props.label === "Math and Science"
-                        ? classes.MathAndSci
-                        : this.props.label === "IT Professionalism"
-                        ? classes.ITProfessionalism
-                        : this.props.label === "Industrial Experience"
-                        ? classes.IndustrialExperience
-                        : this.props.label === "Team Project"
-                        ? classes.TeamProject
-                        : this.props.label === "Unrestricted Electives"
-                        ? classes.UnrestrictedElectives
-                        : this.props.label === "University Level Requirements"
-                        ? classes.UniversityLevelRequirements
-                        : classes.Default
-                }
-            >
-                <div className={classes.container}>
-                    <label className={classes.label}>{this.props.label}</label>
-                    <label className={classes.labelMC}>
-                        <br />
-                        MCs Needed: {this.calculateMcNeeded()}
-                        <br />
-                        MCs Taken: {0}
-                    </label>
-                </div>
-            </Paper>
-        );
-    }
-}
+    return (
+        <Paper
+            elevation={3}
+            className={
+                props.label === "Foundation"
+                    ? classes.Foundation
+                    : props.label === "Focus Area"
+                    ? classes.FocusArea
+                    : props.label === "Math and Science"
+                    ? classes.MathAndSci
+                    : props.label === "IT Professionalism"
+                    ? classes.ITProfessionalism
+                    : props.label === "Industrial Experience"
+                    ? classes.IndustrialExperience
+                    : props.label === "Team Project"
+                    ? classes.TeamProject
+                    : props.label === "Unrestricted Electives"
+                    ? classes.UnrestrictedElectives
+                    : props.label === "University Level Requirements"
+                    ? classes.UniversityLevelRequirements
+                    : classes.Default
+            }
+        >
+            <div className={classes.container}>
+                <label className={classes.label}>{props.label}</label>
+                <label className={classes.labelMC}>
+                    <br />
+                    MCs Needed: {calculateMcNeeded()}
+                    <br />
+                    MCs Taken: {0}
+                </label>
+            </div>
+        </Paper>
+    );
+};
 
 export default Summary;
